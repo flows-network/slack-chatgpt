ba290bd3d89b4e48d25927d892c42866844756b3
Michael Yuan <michael@secondstate.io>
2023-03-02 11:56:52 +0000

    Use ChatGPT API
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,16 @@
 use slack_flows::{listen_to_channel, send_message_to_channel};
-use openai_flows::{CompletionRequest, create_completion};
-use store_flows::{get, set};
-use serde_json::json;
+use openai_flows::{chat_completion};
 
 #[no_mangle]
 pub fn run() {
     listen_to_channel("secondstate", "collaborative-chat", |sm| {
-        let prompt = get("prompt").unwrap_or_default();
-        let prompt = prompt.as_str().unwrap_or_default();
-        if prompt.len() == 0 {
-            send_message_to_channel("secondstate", "collaborative-chat", "Starting a new conversation since the last interaction was over 5 minutes ago".to_owned());
+        let chat_id = "secondstate-collaborative_chat";
+        let c = chat_completion("Agent", chat_id, &sm.text);
+        if let Some(c) = c {
+            if c.new_conversation {
+                send_message_to_channel("secondstate", "collaborative-chat", "Let's start a new conversation!".to_string());
+            }
+            send_message_to_channel("secondstate", "collaborative-chat", c.choice);
         }
-
-        let mut prompt = prompt.to_owned() + " " + &sm.text;
-        if prompt.len() > 2047 {
-            let (_discard, keep) = prompt.split_at(prompt.len() - 2047);
-            prompt = keep.to_owned();
-        }
-        set("prompt", json!(prompt));
-        let cr = CompletionRequest {
-            prompt: prompt,
-            n: 1,
-            max_tokens: 2048,
-            ..Default::default()
-        };
-
-        let r = create_completion("Agent", cr);
-        r.iter().for_each(|c| {
-            send_message_to_channel("secondstate", "collaborative-chat", c.to_string());
-        });
     });
 }
