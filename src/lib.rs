42b13fce32ad3cb1d6223341ba0707630f657b67
Michael Yuan <michael@secondstate.io>
2023-03-13 06:53:44 +0000

    Parameterized the slack target
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,33 @@
 use slack_flows::{listen_to_channel, send_message_to_channel};
 use openai_flows::{chat_completion};
+use std::env;
+use dotenv::dotenv;
 
 #[no_mangle]
 pub fn run() {
-    listen_to_channel("secondstate", "collaborative-chat", |sm| {
-        let chat_id = "secondstate-collaborative_chat";
-        let c = chat_completion("Agent", chat_id, &sm.text);
+    dotenv().ok();
+    let workspace: String = match env::var("workspace") {
+        Err(_) => "secondstate".to_string(),
+        Ok(name) => name,
+    };
+
+    let channel: String = match env::var("channel") {
+        Err(_) => "collaborative-chat".to_string(),
+        Ok(name) => name,
+    };
+    let openai_key_name: String = match env::var("openai_key_name") {
+        Err(_) => "chatmichael".to_string(),
+        Ok(name) => name,
+    };
+
+    listen_to_channel(&workspace, &channel, |sm| {
+        let chat_id = workspace.clone() + &channel;
+        let c = chat_completion(&openai_key_name, &chat_id, &sm.text);
         if let Some(c) = c {
             if c.new_conversation {
-                send_message_to_channel("secondstate", "collaborative-chat", "Let's start a new conversation!".to_string());
+                send_message_to_channel(&workspace, &channel, "Let's start a new conversation!".to_string());
             }
-            send_message_to_channel("secondstate", "collaborative-chat", c.choice);
+            send_message_to_channel(&workspace, &channel, c.choice);
         }
     });
 }
