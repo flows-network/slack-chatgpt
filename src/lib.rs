95a94ec8375e0dc2dcda9a71e6b95895ad39ddc3
Michael Yuan <michael@secondstate.io>
2023-03-13 15:08:53 +0000

    Upgrade to openai sdk 0.2
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use slack_flows::{listen_to_channel, send_message_to_channel};
-use openai_flows::{chat_completion};
+use openai_flows::{chat_completion, ChatOptions};
 use std::env;
 use dotenv::dotenv;
 
@@ -22,9 +22,9 @@ pub fn run() {
 
     listen_to_channel(&workspace, &channel, |sm| {
         let chat_id = workspace.clone() + &channel;
-        let c = chat_completion(&openai_key_name, &chat_id, &sm.text);
+        let c = chat_completion(&openai_key_name, &chat_id, &sm.text, &ChatOptions::default());
         if let Some(c) = c {
-            if c.new_conversation {
+            if c.restarted {
                 send_message_to_channel(&workspace, &channel, "Let's start a new conversation!".to_string());
             }
             send_message_to_channel(&workspace, &channel, c.choice);
